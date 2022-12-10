623d656953acff175a4f40a6331feef6f919631a
Agastya Patri <patriagastya5@gmail.com>
2022-12-10 10:40:37 +0530

    date indexing corrected. correct data needed

--- src/visualize.py
+++ src/visualize.py
@@ -9,16 +9,16 @@ data = os.listdir(PATH)
 
 final_data = pd.DataFrame(columns=[name[:-4] for name in data])
 relevant_dates = pd.read_csv(os.path.join(PATH, data[0]))["DATE"]
-final_data.insert(0, column="DATE", value = relevant_dates)
-dataframes = [pd.read_csv(os.path.join(PATH, d)) for d in data]
+final_data.insert(0, column="DATE", value = pd.read_csv(os.path.join(PATH, data[0]))["DATE"])
+final_data.set_index("DATE", inplace=True)
 
-for df in dataframes:
-    try:
-        # write code to pick out the last 430 elements of the dataframe
-        values = df.iloc[-430:, 1]
-        final_data[values.name] = values 
+for col in final_data.columns:
+    name = col + ".csv"
+    df = pd.read_csv(os.path.join(PATH, name), parse_dates=["DATE"], index_col=["DATE"])
+    try: 
+        values = df.loc["1987-01-01":"2022-09-01"]
+        final_data[col] = values 
     except:
-        pass  
+        continue
 
-print(final_data["DATE", "HSN1F"])
-
+print(final_data)
